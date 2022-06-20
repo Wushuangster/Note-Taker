d1c2a11752fb2f97011dca45b6cfd84bbb6ab23b
wushuangster <wushuangster@hotmail.com>
2022-06-20 02:15:04 -0700

    upload new file to repo

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const api = require('./rotues/index.js');
+
+let port = process.env.PORT || 3001;
+
+const app = express();
+
+//Middleware gor parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for notes.html
+app.get('*' , (req, res) => 
+   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for index.html
+app.get('*' , (req, res) => 
+   res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+// start app using port 3001
+app.listen(port, () =>
+   console.log(`App listening on port ${port}`)
+);
+
