9b43a09c7fca3185583f114c6627736840ed3d63
wushuangster <wushuangster@hotmail.com>
2022-06-21 22:56:03 -0700

    modified index.html

--- helpers/uuid.js
+++
@@ -1,6 +0,0 @@
-// Immediately export a function that generates a string of random numbers and letters
-module.exports = () => 
-   Math.floor((1+ Math,randon()) * 0x10000)
-        .toString(16)
-        .substring(1);
-   
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-//Import our modeular routers for /tips and  /feedback
-const noteRouter = require('./notes.js');
-
-const app = express();
-app.use('/notes', noteRouter);
-module.exports = app;
