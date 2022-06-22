87bbab003417e5de467d1c796e5157ae08d06c86
wushuangster <wushuangster@hotmail.com>
2022-06-21 22:58:39 -0700

    changed some bugs

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+let port = process.env.PORT || 3001;
+
+const app = express();
+
+//Middleware gor parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+// app.use('/api', api);
+// GET Route for notes.html
+app.get('/notes' , (req, res) => 
+   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for index.html
+app.get('*' , (req, res) => 
+   res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+// start app using port 3001
+app.listen(port, () =>
+   console.log(`App listening on port ${port}`)
+);
+
