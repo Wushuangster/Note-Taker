7c95bf4559918a5c764e81cd86d32c7614ed5757
wushuangster <wushuangster@hotmail.com>
2022-06-23 22:14:28 -0700

    modified some bugs

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-// app.use('/api', api);
+app.use('/api', api);
 // GET Route for notes.html
 app.get('/notes' , (req, res) => 
    res.sendFile(path.join(__dirname, '/public/notes.html'))
